96222d8928922aeb34745af2ac19ffd53720727f
mperezmartinez <mperezmartinez@eniac29.danielcastelao.org>
2014-12-04 09:20:56 +0100

    3ºCommit, Cambio de nombre de variables y metodos.

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -7,27 +7,27 @@ public class ExamenCOD {
      */
     public static void main(String[] args) {
         int iN = 11;
-        if (metodoMio(iN)) {
+        if (calcprimo(iN)) {
             System.out.println("Es numero primo");
         } else {
             System.out.println("NO es numero primo");
         }
 
         for (int x = 2; x < 1000; x++) {
-            if (metodoMio(x)) {
+            if (calcprimo(x)) {
                 System.out.print(x + " ");
             }
         }
     }
 
-    public static boolean metodoMio(int variable) {
-        int j = 2;
+    public static boolean calcprimo(int num1) {
+        int i = 2;
         boolean aux = true;
-        while ((aux) && (j != variable)) {
-            if (variable % j == 0) {
+        while ((aux) && (i != num1)) {
+            if (num1 % i == 0) {
                 aux = false;
             }
-            j++;
+            i++;
         }
         return aux;
     }
