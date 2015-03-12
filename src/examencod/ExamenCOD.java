b82a262bada35e1674f55e068ff4afbe3b7149bd
Miguel <Miguel@android-asb153>
2015-03-12 09:07:28 +0100

    Javadoc

--- src/examencod/ExamenCOD.java
+++ src/examencod/ExamenCOD.java
@@ -1,6 +1,10 @@
 
 package examencod;
 
+/**
+ *
+ * @author Miguel
+ */
 public class ExamenCOD {
 
     /**
@@ -8,17 +12,23 @@ public class ExamenCOD {
      */
     public static void main(String[] args) {
  int num = 11;
-            	if (metodoMio(num))
+            	if (parimpar(num))
 System.out.println("Es numero primo");
 else
  System.out.println("NO es numero primo");           	 
           	 
 for (int x=2;x<1000;x++){
-if (metodoMio(x))
+if (parimpar(x))
 System.out.print(x + " ");                         	 
 }         	 
 	}
-public static boolean metodoMio(int var) {
+
+    /**
+     * Devuelve  verdaderi si es par
+     * @param var
+     * @return boolean ver
+     */
+    public static boolean parimpar(int var) {
 int i = 2;
 boolean ver=true;
 while ((ver) && (i!=var)){
